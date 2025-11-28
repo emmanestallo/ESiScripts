729791b50e75294e1028294d1b75c3f10d7126d3
Emmanuel Jesus Estallo <emmanuelestallo14.ee@gmail.com>
2025-11-28 12:20:51 +0800

    Update report_acpc.py

--- report_acpc.py
+++ report_acpc.py
@@ -1,31 +1,36 @@
-import pandas as pd 
-import re 
+import pandas as pd
+import re
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Filter ACPC Violation')
+parser.add_argument('-xmin', type=float, default=0, help='Minimum X coordinate (um)')
+parser.add_argument('-xmax', type=float, default=1000, help='Maximum X coordinate (um)')
+parser.add_argument('-ymin', type=float, default=0, help='Minimum Y coordinate (um)')
+parser.add_argument('-ymax', type=float, default=1000, help='Maximum Y coordinate (um)')
+parser.add_argument('-violation', type=float, default=6, help='Minimum violation threshold')
+args = parser.parse_args()
 
 irms_file = 'xa-VDD_acpc.ascii'
 processed_file = 'cleaned.txt'
 report_file = 'report_acpc_violation.csv'
 
-# adjust coordinates in um
-x_min, y_min = 31, 2 
-x_max, y_max = 250, 20
-
 if not os.path.exists(processed_file):
-    with open(irms_file, 'r') as f, open(processed_file, 'w') as w: 
-        lines = f.readlines() 
-        first_line = lines[0].replace(',','_').replace('/','_')
-        first_line = re.sub(r'(?<!\s) (?!\s)', '_', first_line) 
-        w.write(first_line) 
+    with open(irms_file, 'r') as f, open(processed_file, 'w') as w:
+        lines = f.readlines()
+        first_line = lines[0].replace(',', '_').replace('/', '_')
+        first_line = re.sub(r'(?<!\s) (?!\s)', '_', first_line)
+        w.write(first_line)
         for line in lines[5:]:
             w.write(line)
 
-df = pd.read_csv(processed_file, sep='\s+')
-
+df = pd.read_csv(processed_file, sep=r'\s+')
 
 filtered_data = df[
-    (df['Violation'] >= 6) &
-    (df['X'] >= x_min) & (df['X'] <= x_max) &
-    (df['Y'] >= y_min) & (df['Y'] <= y_max)
+    (df['Violation'] >= args.violation) &
+    (df['X'] >= args.xmin) & (df['X'] <= args.xmax) &
+    (df['Y'] >= args.ymin) & (df['Y'] <= args.ymax)
 ]
 
 filtered_data.to_csv(report_file, index=False, header=True)
+print(f"Filtered {len(filtered_data)} rows to {report_file}")
